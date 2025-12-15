f5910380043b202b51fef8ed05e672600401c89b
roni2026 <hridoyjsb@gmail.com>
2025-12-15 12:57:09 +0600

    Update route.ts

--- app/api/send-invoice/route.ts
+++ app/api/send-invoice/route.ts
@@ -27,7 +27,7 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       host: 'smtp-relay.brevo.com',
       port: 465,
-      secure: false, // true for 465, false for other ports
+      secure: true, // true for 465, false for other ports
       auth: {
         user: '9d0a00001@smtp-brevo.com', // Your Brevo Login
         pass: process.env.BREVO_SMTP_KEY, // STORE THIS IN .env.local FILE!
@@ -67,3 +67,4 @@ export async function POST(req: Request) {
   }
 
 }
+
